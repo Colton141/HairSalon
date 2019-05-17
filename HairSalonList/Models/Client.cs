4682942daab1a762c3bf44ac05a989b47bb8ba99
colton lacey <coltnutmiles14@gmail.com>
2019-05-17 10:49:17 -0700

    add delete all method for clients

--- HairSalonList/Models/Client.cs
+++ HairSalonList/Models/Client.cs
@@ -181,6 +181,19 @@ namespace HairSalonList.Models
         conn.Dispose();
       }
     }
+    public static void DeleteAll()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"DELETE * FROM clients;";
+      cmd.ExecuteNonQuery();
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
 
   }
 }
